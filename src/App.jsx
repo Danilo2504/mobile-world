cecbf6d26a75065f5aeb0581cf7ef2254c72c72d
danilo2504 <danilobautista2004@gmail.com>
2024-03-31 20:15:38 +0200

    fix AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router/appRouter";
 import { CartProvider } from "./contexts/CartContext";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
